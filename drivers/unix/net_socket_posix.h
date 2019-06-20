8276bfbeed075811ef4dab8afe72a03f2a718ee3
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2019-06-20 11:36:32 +0200

    Multicast support in NetSocket/PacketPeerUDP

--- drivers/unix/net_socket_posix.h
+++ drivers/unix/net_socket_posix.h
@@ -60,6 +60,7 @@ private:
 
 	NetError _get_socket_error();
 	void _set_socket(SOCKET_TYPE p_sock, IP::Type p_ip_type, bool p_is_stream);
+	_FORCE_INLINE_ Error _change_multicast_membership(IP_Address p_ip, IP_Address p_if_address, bool p_add);
 
 protected:
 	static NetSocket *_create_func();
@@ -93,6 +94,8 @@ public:
 	virtual void set_tcp_no_delay_enabled(bool p_enabled);
 	virtual void set_reuse_address_enabled(bool p_enabled);
 	virtual void set_reuse_port_enabled(bool p_enabled);
+	virtual Error add_multicast_membership(const IP_Address &p_multi_address, IP_Address p_if_address);
+	virtual Error drop_multicast_membership(const IP_Address &p_multi_address, IP_Address p_if_address);
 
 	NetSocketPosix();
 	~NetSocketPosix();

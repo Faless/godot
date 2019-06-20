50e3898472864915ad8dfabc1f0cdb0a4f47a266
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2019-06-20 17:23:31 +0200

    more explicit BSD

--- drivers/unix/net_socket_posix.cpp
+++ drivers/unix/net_socket_posix.cpp
@@ -61,8 +61,9 @@
 #endif
 
 // BSD calls this flag IPV6_JOIN_GROUP
-#if !defined(IPV6_ADD_MEMBERSHIP) && defined(IPV6_JOIN_GROUP)
 #define IPV6_ADD_MEMBERSHIP IPV6_JOIN_GROUP
+#endif
+#if !defined(IPV6_DROP_MEMBERSHIP) && defined(IPV6_LEAVE_GROUP)
 #define IPV6_DROP_MEMBERSHIP IPV6_LEAVE_GROUP
 #endif
 

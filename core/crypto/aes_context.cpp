e442511f3247d2ced7deca1df1fda9a50535d4d2
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2020-05-25 08:38:16 +0200

    Add AES cbc mode.

--- core/crypto/aes_context.cpp
+++ core/crypto/aes_context.cpp
@@ -62,11 +62,43 @@ PoolByteArray AESContext::decrypt_ecb(PoolByteArray p_src) {
 	return out;
 }
 
+Array AESContext::encrypt_cbc(PoolByteArray p_src, PoolByteArray p_iv) {
+	ERR_FAIL_COND_V_MSG(p_src.size() % 16, Array(), "Encryption source must be in blocks of 16 bytes. Try to add some padding.");
+	ERR_FAIL_COND_V_MSG(p_iv.size() != 16, Array(), "The initialization vector (IV) must be exactly 16 bytes.");
+	PoolByteArray dest;
+	dest.resize(p_src.size());
+	PoolByteArray iv;
+	iv.append_array(p_iv);
+	Error err = ctx.encrypt_cbc(p_src.size(), iv.write().ptr(), p_src.read().ptr(), dest.write().ptr());
+	ERR_FAIL_COND_V(err != OK, Array());
+	Array out;
+	out.push_back(dest);
+	out.push_back(iv);
+	return out;
+}
+
+Array AESContext::decrypt_cbc(PoolByteArray p_src, PoolByteArray p_iv) {
+	ERR_FAIL_COND_V_MSG(p_src.size() % 16, Array(), "Malformed decryption source. Must be in blocks of 16 bytes");
+	ERR_FAIL_COND_V_MSG(p_iv.size() != 16, Array(), "The initialization vector (IV) must be exactly 16 bytes.");
+	PoolByteArray dest;
+	dest.resize(p_src.size());
+	PoolByteArray iv;
+	iv.append_array(p_iv);
+	Error err = ctx.decrypt_cbc(p_src.size(), iv.write().ptr(), p_src.read().ptr(), dest.write().ptr());
+	ERR_FAIL_COND_V(err != OK, Array());
+	Array out;
+	out.push_back(dest);
+	out.push_back(iv);
+	return out;
+}
+
 void AESContext::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_encode_key", "key"), &AESContext::set_encode_key);
 	ClassDB::bind_method(D_METHOD("set_decode_key", "key"), &AESContext::set_decode_key);
 	ClassDB::bind_method(D_METHOD("encrypt_ecb", "src"), &AESContext::encrypt_ecb);
 	ClassDB::bind_method(D_METHOD("decrypt_ecb", "src"), &AESContext::decrypt_ecb);
+	ClassDB::bind_method(D_METHOD("encrypt_cbc", "src", "iv"), &AESContext::encrypt_cbc);
+	ClassDB::bind_method(D_METHOD("decrypt_cbc", "src", "iv"), &AESContext::decrypt_cbc);
 }
 
 AESContext::AESContext() {

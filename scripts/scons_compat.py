e5848adff48a1935b985c22509e30ecb64d4e1dc
Marios Staikopoulos <marios@staik.net>
2020-12-14 21:28:44 -0800

    Meson initial commit
    Finished all modules on except for mono and webm.
    Windows compilation with MSVC (LLVM-mingw kind of)
    Linux compilation with GCC
    Linux cross compile to windows with gcc-mingw-64

---
+++ scripts/scons_compat.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+import os, sys, importlib.util
+from importlib.machinery import SourceFileLoader
+from utils import fix_path
+
+
+def run(script, method, source, target):
+    pass
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("method")
+    parser.add_argument("script")
+    parser.add_argument("-s", "--source-fix-path", default="")
+    parser.add_argument("-t", "--target-fix-path", default="")
+    parser.add_argument("-i", "--inputs", nargs="+", default=[])
+    parser.add_argument("-o", "--outputs", nargs="+", default=[])
+    parser.add_argument("-e", "--scons-env", nargs="+", default=[])
+
+    args = parser.parse_args()
+
+    script = args.script
+    if "MESON_SOURCE_ROOT" in os.environ:
+        sys.path.append(os.getenv("MESON_SOURCE_ROOT"))
+        script = os.path.join(os.getenv("MESON_SOURCE_ROOT"), script)
+
+    sources = []
+    if args.source_fix_path:
+        for s in args.inputs:
+            sources.append(fix_path(s, args.source_fix_path))
+    else:
+        sources = args.inputs
+
+    targets = []
+    if args.target_fix_path:
+        for t in args.outputs:
+            print(t)
+            targets.append(fix_path(t, args.target_fix_path))
+    else:
+        targets = args.outputs
+
+    env = None
+    if args.scons_env:
+        env = dict()
+    for v in args.scons_env:
+        key, val = v.split("=", 1)
+        if val == "true":
+            val = True
+        elif val == "false":
+            val = False
+        env[key] = val
+
+    mymodule = SourceFileLoader("script", script).load_module()
+    getattr(mymodule, args.method)(targets, sources, env)

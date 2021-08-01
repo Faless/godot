7c75d46c6b733c4c61d703d3f5c30b445548960c
Jussi Pakkanen <jpakkane@gmail.com>
2021-08-01 17:53:22 +0300

    Fixes needed to get compilation started on current master.

--- modules/webxr/config.py
+++ modules/webxr/config.py
@@ -1,6 +1,9 @@
-def can_build(env, platform):
-    return not env["disable_3d"]
+def can_build(env):
+    return True#not env["disable_3d"]
 
+
+def get_name():
+    return 'webxr'
 
 def configure(env):
     pass

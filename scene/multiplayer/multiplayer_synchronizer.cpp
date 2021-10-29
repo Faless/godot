b5f9094b13f155ff53addafc4f79f8cfa087f103
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2021-10-29 17:54:44 +0200

    More sync fun.

--- scene/multiplayer/multiplayer_synchronizer.cpp
+++ scene/multiplayer/multiplayer_synchronizer.cpp
@@ -2,9 +2,14 @@
 
 void MultiplayerSynchronizer::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("sync"), &MultiplayerSynchronizer::sync);
+
 	ClassDB::bind_method(D_METHOD("set_auto_sync_enabled", "enabled"), &MultiplayerSynchronizer::set_auto_sync_enabled);
 	ClassDB::bind_method(D_METHOD("is_auto_sync_enabled"), &MultiplayerSynchronizer::is_auto_sync_enabled);
 	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "auto_sync"), "set_auto_sync_enabled", "is_auto_sync_enabled");
+
+	ClassDB::bind_method(D_METHOD("set_replication_config", "config"), &MultiplayerSynchronizer::set_replication_config);
+	ClassDB::bind_method(D_METHOD("get_replication_config"), &MultiplayerSynchronizer::get_replication_config);
+	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "resource", PROPERTY_HINT_RESOURCE_TYPE, "SceneReplicationConfig"), "set_replication_config", "get_replication_config");
 }
 
 Error MultiplayerSynchronizer::sync() {
@@ -23,4 +28,12 @@ void MultiplayerSynchronizer::_notification(int p_what) {
 	if (p_what == NOTIFICATION_ENTER_TREE) {
 		// TODO will need to advertize this to the API, so we know how to spawn.
 	}
+}
+
+void MultiplayerSynchronizer::set_replication_config(Ref<SceneReplicationConfig> p_config) {
+	replication_config = p_config;
+}
+
+Ref<SceneReplicationConfig> MultiplayerSynchronizer::get_replication_config() {
+	return replication_config;
 }
--- scene/multiplayer/multiplayer_synchronizer.h
+++ scene/multiplayer/multiplayer_synchronizer.h
@@ -1,19 +1,26 @@
 #include "scene/main/node.h"
 
+#include "scene/resources/scene_replication_config.h"
+
 class MultiplayerSynchronizer : public Node {
 	GDCLASS(MultiplayerSynchronizer, Node);
 
 private:
 	bool auto_sync = true;
+	Ref<SceneReplicationConfig> replication_config;
 
 protected:
 	static void _bind_methods();
 	void _notification(int p_what);
 
 public:
+	Error sync();
+
 	void set_auto_sync_enabled(bool p_enabled);
 	bool is_auto_sync_enabled() const;
-	Error sync();
+
+	void set_replication_config(Ref<SceneReplicationConfig> p_config);
+	Ref<SceneReplicationConfig> get_replication_config();
 
 	MultiplayerSynchronizer() {}
 };

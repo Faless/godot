a4df233e1e76d3195641372911f4ee5b2b33339c
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2026-09-04 19:21:11 +0200

    web: Implement Web Editor debugger via MessagePort

--- platform/web/js/libs/library_godot_os.js
+++ platform/web/js/libs/library_godot_os.js
@@ -61,6 +61,7 @@ const GodotConfig = {
 		canvas_resize_policy: 2, // Adaptive
 		virtual_keyboard: false,
 		godot_pool_size: 4,
+		debug_port: null,
 		on_execute: null,
 		on_exit: null,
 
@@ -70,6 +71,7 @@ const GodotConfig = {
 			GodotConfig.locale = p_opts['locale'] || GodotConfig.locale;
 			GodotConfig.virtual_keyboard = p_opts['virtualKeyboard'];
 			GodotConfig.godot_pool_size = p_opts['godotPoolSize'];
+			GodotConfig.debug_port = p_opts['debugPort'];
 			GodotConfig.on_execute = p_opts['onExecute'];
 			GodotConfig.on_exit = p_opts['onExit'];
 			if (p_opts['focusCanvas']) {
@@ -80,11 +82,13 @@ const GodotConfig = {
 		locate_file: function (file) {
 			return Module['locateFile'](file);
 		},
+
 		clear: function () {
 			GodotConfig.canvas = null;
 			GodotConfig.locale = 'en';
 			GodotConfig.canvas_resize_policy = 2;
 			GodotConfig.virtual_keyboard = false;
+			GodotConfig.debug_port = null;
 			GodotConfig.on_execute = null;
 			GodotConfig.on_exit = null;
 		},
